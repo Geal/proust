bc843fb5256224f58f16d1e642657db021ad5486
Clement Delafargue <clement@delafargue.name>
2015-05-21 15:37:00 +0200

    Request Parser
    + basic test for TopicMetadataRequest
    ToDo: implement all the payload parsers

--- src/parser/request.rs
+++ src/parser/request.rs
@@ -10,14 +10,16 @@ use nom::Err::*;
 
 use parser::metadata::*;
 
+#[derive(PartialEq,Debug)]
 pub struct RequestMessage<'a> {
     api_version: i16,
     correlation_id: i32,
     client_id: &'a [u8],
-    request_payload: &'a RequestPayload<'a>
+    request_payload: RequestPayload<'a>
 }
 
 // ToDo other requests
+#[derive(PartialEq,Debug)]
 pub enum RequestPayload<'a> {
     MetadataRequest(TopicMetadataRequest<'a>)
 }
@@ -41,24 +43,47 @@ pub fn parse_request_payload<'a>(api_key: i16, input:&'a [u8]) -> IResult<&'a [u
     }
 }
 
-// ToDo understand why the chain! macro doesn't work
-/*
-pub fn request_message<'a>(input:&'a [u8]) -> IResult<&'a [u8], &'a RequestMessage<'a>> {
+pub fn request_message<'a>(input:&'a [u8]) -> IResult<&'a [u8], RequestMessage<'a>> {
     chain!(
       input,
       key: be_i16 ~
       version: be_i16 ~
       correlation_id: be_i32 ~
       client_id: kafka_string ~
-      payload: topic_metadata_request, || {
-          & RequestMessage {
-              api_key: key,
+      payload: call!(|i| { parse_request_payload(key, i) }), || {
+          RequestMessage {
               api_version: version,
               correlation_id: correlation_id,
               client_id: client_id,
-              request_payload: & RequestPayload::MetadataRequest(payload)
+              request_payload: payload
           }
       }
     )
 }
-*/
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use nom::*;
+  use nom::IResult::*;
+
+  #[test]
+  fn request_message_test() {
+      let input = &[
+        0x00, 0x03,             // api_key = 3
+        0x00, 0x00,             // api_version = 0
+        0x00, 0x00, 0x00, 0x00, // correlation_id = 0
+        0x00, 0x00,             // client_id = ""
+        0x00, 0x00, 0x00, 0x00  // request_payload = []
+      ];
+      let result = request_message(input);
+      let expected = RequestMessage {
+        api_version: 0,
+        correlation_id: 0,
+        client_id: &[][..],
+        request_payload: RequestPayload::MetadataRequest(vec![])
+      };
+
+      assert_eq!(result, Done(&[][..], expected))
+  }
+}
