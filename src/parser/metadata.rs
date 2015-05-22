ed424e48a7df9565d085b008a909ad845960d195
Clement Delafargue <clement@delafargue.name>
2015-05-22 18:31:21 +0200

    Test for TopicMetadata parser

--- src/parser/metadata.rs
+++ src/parser/metadata.rs
@@ -3,7 +3,7 @@
 
 use parser::primitive::*;
 
-use nom::{HexDisplay,Needed,IResult,FileProducer,be_u8,be_u16,be_u32,be_u64,be_f32};
+use nom::{HexDisplay,Needed,IResult,FileProducer};
 use nom::{Consumer,ConsumerState};
 use nom::IResult::*;
 use nom::Err::*;
@@ -12,4 +12,22 @@ pub type TopicMetadataRequest<'a> = Vec<KafkaString<'a>>;
 
 pub fn topic_metadata_request<'a>(input:&'a [u8]) -> IResult<&'a [u8], TopicMetadataRequest<'a>> {
   kafka_array(input, kafka_string)
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use nom::*;
+  use nom::IResult::*;
+
+  #[test]
+  fn topic_metadata_request_tests() {
+      let input = &[
+        0x00, 0x00, 0x00, 0x01, 0x00, 0x00  //  [""]
+      ];
+      let result = topic_metadata_request(input);
+      let expected = vec![&[][..]];
+
+      assert_eq!(result, Done(&[][..], expected))
+  }
 }
