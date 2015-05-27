2d60dd98e19b1dbc2217e20d4af52b3838c9be20
Geoffroy Couprie <geo.couprie@gmail.com>
2015-05-27 18:53:09 +0200

    Example async TCP usage

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,42 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
-#![feature(core)]
+#![feature(core,std_misc)]
 extern crate core;
 extern crate mmap;
+extern crate mio;
 
 #[macro_use]
 extern crate nom;
 
 use parser::request::*;
+use std::error::Error;
 
 mod parser;
 mod storage;
+mod network;
+
 
 fn main() {
   storage::storage_test();
+
+  let rx = network::start_listener("abcd");
+  loop {
+    select! {
+      val  = rx.recv() => match val {
+        Ok(a)  => println!("Received val {}", a),
+        Err(e) => {
+          println!("Received err {:?}", e.description());
+          break;
+        }
+      }
+      /*,
+      () = timeout.recv() => {
+        println!("timed out, total time was more than 10 seconds");
+        break;
+      }*/
+    }
+  }
+
   println!("Hello, world!");
 }
 
