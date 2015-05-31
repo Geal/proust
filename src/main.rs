6b95e640b751be340d8103ac3e620ce0f0aca439
Geoffroy Couprie <geo.couprie@gmail.com>
2015-05-31 11:58:21 +0200

    it kinda work, but not complete

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,6 @@ mod network;
 mod responses;
 use responses::response::*;
 
-mod parser;
 mod storage;
 mod network;
 mod util;
--- src/network.rs
+++ src/network.rs
@@ -3,18 +3,28 @@ use std::string::String;
 use std::sync::mpsc;
 use std::sync::mpsc::{channel,Receiver};
 use std::rt::unwind::try;
+
+use std::str;
+use std::io::Read;
+//use std::net::TcpStream;
+use mio::tcp::*;//{TcpListener};
 use mio::*;
-use mio::tcp::{TcpListener, TcpStream};
+use mio::buf::{ByteBuf};
 use util::monitor;
 use storage::{self,storage};
 use std::marker::PhantomData;
 
 const SERVER: Token = Token(0);
+const CLIENT: Token = Token(1);
 
 #[derive(Debug)]
 pub enum Message {
   Stop,
   Data(Vec<u8>)
+}
+
+struct Client {
+  sock: NonBlock<TcpStream>
 }
 
 pub struct ListenerMessage {
@@ -27,9 +37,11 @@ pub struct Listener {
 }
 
 pub struct MyHandler {
-  listener:    TcpListener,
+  listener:    NonBlock<TcpListener>,
   storage_tx : mpsc::Sender<storage::Request>,
-  counter:     u8
+  counter:     u8,
+  token_index: usize,
+  clients:     Vec<NonBlock<TcpStream>>
 }
 
 
@@ -38,36 +50,84 @@ impl Handler for MyHandler {
   type Message = Message;
 
   fn readable(&mut self, event_loop: &mut EventLoop<MyHandler>, token: Token, _: ReadHint) {
+    println!("readable");
     match token {
       SERVER => {
         let s = self as &mut MyHandler;
-        let _ = s.listener.accept();
-        println!("accepted message");
-        s.storage_tx.send(s.counter);
-        s.counter = s.counter + 1;
-        if s.counter > 1 {
+        if let Ok(Some(stream)) = s.listener.accept() {
+          println!("got client n°{:?}", s.token_index);
+          //s.storage_tx.send(s.counter)
+          //let index = s.token_index;
+          //let token = Token(index);
+          //s.token_index += 1;
+          let i = s.clients.len() +1;
+          let token = Token(i);
+          event_loop.register_opt(&stream, token, Interest::all(), PollOpt::edge());
+          event_loop.register(&stream, token);
+          s.clients.push(stream);
+          /*s.counter = s.counter + 1;
+          if s.counter > 1 {
           panic!();
+          }*/
+        } else {
+          println!("invalid connection");
         }
+      },
+      Token(x) => {
+        println!("client n°{:?} readable", x);
+        let client = &mut self.clients[x - 1];
+        //let mut buf = buf::ByteBuf::mut_with_capacity(1024);
+        // let mut v: Vec<u8> = Vec::with_capacity(2048);
+        let mut read_buf = ByteBuf::mut_with_capacity(2048);
+        if let Ok(_) =  client.read(&mut read_buf) {
+          let mut buf = read_buf.flip();
+          let mut text = String::new();
+          buf.read_to_string(&mut text);
+          println!("Received: {}", text);
+        }
+        //let mut sl = [0; 2048];
+        //read_buf.read_slice(sl);
+        //println!("read: {:?}", str::from_utf8(sl));
       }
-      _ => panic!("unexpected token"),
+      //_ => panic!("unexpected token"),
+    }
+  }
+
+  fn writable(&mut self, event_loop: &mut EventLoop<Self>, token: Token) {
+    println!("writable");
+    match token {
+      SERVER => {
+        println!("server writeable");
+      },
+      Token(x) => {
+        println!("client n°{:?} writeable", x);
+      }
     }
   }
 
   fn notify(&mut self, _reactor: &mut EventLoop<MyHandler>, msg: Message) {
-    println!("message: {:?}", msg);
+    println!("notify: {:?}", msg);
   }
 
+
+  fn timeout(&mut self, event_loop: &mut EventLoop<Self>, timeout: Self::Timeout) {
+    println!("timeout");
+  }
+
+  fn interrupted(&mut self, event_loop: &mut EventLoop<Self>) {
+    println!("interrupted");
+  }
 }
 
 pub fn start_listener(address: &str) -> (Sender<Message>,thread::JoinHandle<()>)  {
   let mut event_loop = EventLoop::new().unwrap();
   let t2 = event_loop.channel();
   let jg = thread::spawn(move || {
-    let listener = TcpListener::bind("127.0.0.1:4242").unwrap();
+    let listener = NonBlock::new(TcpListener::bind("127.0.0.1:4242").unwrap());
     event_loop.register(&listener, SERVER).unwrap();
     let t = storage(&event_loop.channel(), "pouet");
 
-    event_loop.run(&mut MyHandler {listener: listener, storage_tx: t, counter: 0}).unwrap();
+    event_loop.run(&mut MyHandler {listener: listener, storage_tx: t, counter: 0, token_index: 0, clients: Vec::new()}).unwrap();
 
   });
 
